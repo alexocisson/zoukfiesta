6e64973e4ed0e8ba49b8d82e4d51078a7949cda0
mathias.marty <mathias.marty@he-arc.ch>
2020-10-08 09:02:58 +0200

    Add network interface for client

---
+++ code/app/src/main/java/ch/hearc/zoukfiesta/nearby/utils/INearbyClient.kt
@@ -0,0 +1,21 @@
+package ch.hearc.zoukfiesta.nearby.utils
+
+interface INearbyClient {
+
+    //Send
+    fun sendSkip(musicName : String)
+    fun sendWhat()
+    fun sendMusics()
+    fun sendAdd(musicName : String)
+    
+    //Receive
+    fun onPlaylist(
+        lmbd:
+            (playlist : Array<String>,
+             votes : Map<String, UInt>,
+             currentlyPlaying : String,
+             currentMusicTime : Float,
+            currentMusicLength : Float) -> Void
+    )
+    fun onKick(lmbd : () -> Void)
+}

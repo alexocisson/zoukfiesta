7373eac85d5bb5e470c134d13dad186acbcca630
mathias.marty <mathias.marty@he-arc.ch>
2020-11-21 15:34:24 +0100

    Add music list in host activity

--- code/app/src/main/java/ch/hearc/zoukfiesta/activity/ZoukHostActivity.kt
+++ code/app/src/main/java/ch/hearc/zoukfiesta/activity/ZoukHostActivity.kt
@@ -9,12 +9,6 @@ class ZoukHostActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Player.play(this, R.raw.apareka)
-
-        Thread.sleep(5000)
-
-        Player.play(this, R.raw.dililme)
-
         setContentView(R.layout.zouk_host_activity)
     }
 }

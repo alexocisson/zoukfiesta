df4f1a1a0c38535e09183408fd124cc47dba439c
mathias.marty <mathias.marty@he-arc.ch>
2020-11-21 14:59:57 +0100

    Add music player and somes mp3's

--- code/app/src/main/java/ch/hearc/zoukfiesta/activity/ZoukHostActivity.kt
+++ code/app/src/main/java/ch/hearc/zoukfiesta/activity/ZoukHostActivity.kt
@@ -3,10 +3,17 @@ package ch.hearc.zoukfiesta.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ch.hearc.zoukfiesta.R
+import ch.hearc.zoukfiesta.utils.music.Player
 
 class ZoukHostActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Player.play(this, R.raw.apareka)
+
+        Thread.sleep(5000)
+
+        Player.play(this, R.raw.dililme)
 
         setContentView(R.layout.zouk_host_activity)
     }
